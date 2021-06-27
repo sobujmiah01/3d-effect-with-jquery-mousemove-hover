b84f8b5b15025c907720586b2185fb2323f8d49b
Sobuj Miah <72043862+md-sobuj-miah@users.noreply.github.com>
2021-06-28 00:27:03 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var card = $(".card");
+
+$(document).on("mousemove",function(e) {  
+  var ax = -($(window).innerWidth()/2- e.pageX)/20;
+  var ay = ($(window).innerHeight()/2- e.pageY)/10;
+  card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
+});
